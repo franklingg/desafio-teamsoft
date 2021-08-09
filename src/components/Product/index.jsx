08cde85b2d968f9beb8c8b0e9a6dc9f8716cc3fd
franklingg <franklin.oliveira@ccc.ufcg.edu.br>
2021-08-09 13:57:45 -0300

    Consertando imagem quebrada de burger

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -3,7 +3,7 @@ import './Product.css';
 export function Product({productInfo}){
     return(
         <div className="product">
-            <img className="product__image" alt={productInfo.nm_product} src={productInfo.url_image}/>
+            <img className="product__image" alt={productInfo.nm_product} src={'/assets/burger.png'}/>
             <h1 className="product__title">{productInfo.nm_product}</h1>
             <p className="product__description">{productInfo.description}</p>
             <div className="product__prices">
