9dacc8a36e00d17b4e289b82a341418215bc9279
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-12-19 17:38:44 -0300

    altera imagem de burger

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -3,19 +3,20 @@ import {useState, useEffect} from 'react';
 import { Ordering } from '../../components/Ordering';
 import {Product} from '../../components/Product'
 import './Order.css';
+import orderJSON from '../../data/products.json';
 
 // Using JSON Server on port 3333 (yarn server)
 const API_URL = "http://localhost:3333/products/1";
 
 export function Order() {
 
-    const [orderInfo, setOrderInfo] = useState(null);
+    const [orderInfo] = useState(orderJSON.products[0]);
 
-    useEffect(()=>{
-        fetch(API_URL)
-        .then(response=>response.json())
-        .then(data=>setOrderInfo(data));
-    },[]);
+    // useEffect(()=>{
+    //     fetch(API_URL)
+    //     .then(response=>response.json())
+    //     .then(data=>setOrderInfo(data));
+    // },[]);
 
     return(
             <main className="order">
